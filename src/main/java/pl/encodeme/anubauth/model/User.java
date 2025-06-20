0bedd8d00d80404c379b3c80c2134b7a29106758
Arkadiusz Sas <kontakt.sas.arkadiusz@gmail.com>
2025-06-20 23:58:39 +0200

    Remove password generation features

--- src/main/java/pl/encodeme/anubauth/model/User.java
+++ src/main/java/pl/encodeme/anubauth/model/User.java
@@ -4,15 +4,10 @@ import lombok.Builder;
 
 import java.util.List;
 
-// @TODO: Add models for Username, Email, Password, and Role
 @Builder
 public record User(UserId userId,
                    String username,
                    String email,
                    String password,
                    List<String> roles) {
-
-    public User withEncryptedPassword(String encryptedPassword) {
-        return new User(userId, username, email, encryptedPassword, roles);
-    }
 }
--- src/main/java/pl/encodeme/anubauth/user/UserAuthRepository.java
+++
@@ -1,8 +0,0 @@
-package pl.encodeme.anubauth.user;
-
-import pl.encodeme.anubauth.model.User;
-
-public interface UserAuthRepository {
-    User findByEmail(String email);
-    User saveOrUpdate(User user);
-}
