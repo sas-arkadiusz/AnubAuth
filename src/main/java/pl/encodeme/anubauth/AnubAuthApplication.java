a71930df9479a6a24865507aa0fbc01d9f24a3ec
Arkadiusz Sas <kontakt.sas.arkadiusz@gmail.com>
2025-06-20 20:42:56 +0200

    Init the project structure

---
+++ src/main/java/pl/encodeme/anubauth/AnubAuthApplication.java
@@ -0,0 +1,13 @@
+package pl.encodeme.anubauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnubAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnubAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/encodeme/anubauth/AnubAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.encodeme.anubauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnubAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
